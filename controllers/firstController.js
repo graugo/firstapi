f5b6f20249cf1879ba9e1cd6fd1791d4671c5e70
jordi <jordigraurios@gmail.com>
2021-10-05 19:50:45 +0200

    again

--- controllers/firstController.js
+++ controllers/firstController.js
@@ -1,7 +1,7 @@
 module.exports.helloWorld = function (req, res) {
     const alumnes = [`Jordi`, `Marc`, `Keen`, `Jordi`, `Xavi`, `Marcos`, `Marcos`, `Dani`, `Pau`, `David`, `Alba`, `Marc`];
     const total = alumnes.length;
-    const responseObj = {alumnes: `alumnes`, total: `total`};
+    const responseObj = {'alumnes': alumnes, 'total': total};
     const responseStatus = 200;
     res.status(responseStatus).send(responseObj);
 }
